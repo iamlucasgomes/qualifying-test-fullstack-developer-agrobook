561d2893f72aa0789643be76a5f4266990c2726f
iamlucasgomes <lucas.devjs@gmail.com>
2023-05-27 20:33:27 -0300

    feat: Adds validations to book registration

--- src/app/api/books/route.ts
+++ src/app/api/books/route.ts
@@ -14,15 +14,38 @@ import { NextResponse } from 'next/server';
 
 export async function POST(request: Request) {
   const body = await request.json();
+  if (!body.nome || !body.data_lancamento || !body.categoria) {
+    return new NextResponse(JSON.stringify({ message: 'O livro deve ter nome, data de lançamento e categoria' }), {
+      status: 400,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  }
+  if (body.autores.length === 0) {
+    return new NextResponse(JSON.stringify({ message: 'O livro deve ter pelo menos um autor' }), {
+      status: 400,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  }
   const newAuthor = await createBook(body);
-  if (newAuthor.status === 'error') {
-    return new NextResponse(JSON.stringify(newAuthor), {
+  if (newAuthor.status === 'error409') {
+    return new NextResponse(JSON.stringify({ message: newAuthor.message }), {
       status: 409,
       headers: {
         "Content-Type": "application/json",
       },
     });
-  } else if (newAuthor.status === 'success'){
+  } else if (newAuthor.status === 'error404') {
+    return new NextResponse(JSON.stringify({ message: newAuthor.message }), {
+      status: 404,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+  } else if (newAuthor.status === 'success') {
     return new NextResponse(JSON.stringify(newAuthor), {
       status: 201,
       headers: {
