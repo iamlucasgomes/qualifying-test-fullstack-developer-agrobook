784f40f02138169c42615e7934bc0ff2a5c4756f
iamlucasgomes <lucas.devjs@gmail.com>
2023-05-27 20:22:40 -0300

    feat: Add validation for author in book registration

--- src/app/api/books/services/service.ts
+++ src/app/api/books/services/service.ts
@@ -1,59 +1,3 @@
-// import prisma from "../../database/config/prismaClient";
-// import BookData from "../interfaces/BookData.interface";
-
-// export async function createBook(bookData: BookData) {
-//   const { nome, data_lancamento, categoria, autores } = bookData;
-
-//   const existingBook = await prisma.livros.findFirst({
-//     where: {
-//       nome,
-//       data_lancamento: {
-//         equals: new Date(data_lancamento),
-//       },
-//       categoria,
-//     },
-//   });
-
-//   if (existingBook) {
-//     return { status: 'error', message: 'Livro já existe no banco de dados' };
-//   }
-
-//   let newBook;
-//   try {
-//     await prisma.$transaction(async (prisma) => {
-//       const createdBook = await prisma.livros.create({
-//         data: {
-//           nome,
-//           data_lancamento: new Date(data_lancamento),
-//           categoria,
-//         },
-//       });
-
-//       const bookId = createdBook.id;
-
-//       const authorPromises = autores.map((autorId:number) =>
-//         prisma.autoresLivros.create({
-//           data: {
-//             autor: { connect: { id: autorId } },
-//             livro: { connect: { id: bookId } },
-//           },
-//         })
-//       );
-
-//       await Promise.all(authorPromises);
-
-//       newBook = await prisma.livros.findUnique({
-//         where: { id: bookId },
-//         include: { autoresLivros: true },
-//       });
-//     });
-//   } catch (error: unknown) {
-//     return { status: 'error', message: error };
-//   }
-
-//   return { status: 'success', book: newBook };
-// }
-
 import prisma from "../../database/config/prismaClient";
 import BookData from "../interfaces/BookData.interface";
 
@@ -63,13 +7,19 @@ export async function createBook(bookData: BookData) {
   const existingBook = await findBookByDetails(nome, data_lancamento, categoria);
 
   if (existingBook) {
-    return { status: 'error', message: 'Livro já existe no banco de dados' };
+    return { status: 'error409', message: 'Livro já existe no banco de dados' };
+  }
+
+  const validAuthors = await validateAuthors(autores);
+
+  if (validAuthors.length !== autores.length) {
+    return { status: 'error404', message: 'Um ou mais autores não existem no banco de dados' };
   }
 
   try {
     const createdBook = await createNewBook(nome, data_lancamento, descricao, categoria);
     const bookId = createdBook.id;
-    await connectAuthorsToBook(bookId, autores);
+    await connectAuthorsToBook(bookId, validAuthors);
     const newBook = await findBookById(bookId);
     return { status: 'success', book: newBook };
   } catch (error: unknown) {
@@ -89,22 +39,36 @@ async function findBookByDetails(nome: string, data_lancamento: string, categori
   });
 }
 
+async function validateAuthors(authors: number[]) {
+  const validAuthors = await prisma.autores.findMany({
+    where: {
+      id: {
+        in: authors,
+      },
+    },
+    select: {
+      id: true,
+    },
+  });
+
+  return validAuthors.map((author) => author.id);
+}
+
 async function createNewBook(nome: string, data_lancamento: string, descricao: string | undefined, categoria: string) {
   return await prisma.livros.create({
     data: {
       nome,
       data_lancamento: new Date(data_lancamento),
-      descricao,
       categoria,
     },
   });
 }
 
-async function connectAuthorsToBook(bookId: number, autores: number[]) {
-  const authorPromises = autores.map((autorId: number) =>
+async function connectAuthorsToBook(bookId: number, authors: number[]) {
+  const authorPromises = authors.map((authorId: number) =>
     prisma.autoresLivros.create({
       data: {
-        autor: { connect: { id: autorId } },
+        autor: { connect: { id: authorId } },
         livro: { connect: { id: bookId } },
       },
     })
