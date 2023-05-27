629ca30a72ec6b1123c5e016acd4b65021270ef8
iamlucasgomes <lucas.devjs@gmail.com>
2023-05-27 17:46:41 -0300

    feat: Create author route

--- src/app/api/authors/route.ts
+++ src/app/api/authors/route.ts
@@ -1,4 +1,4 @@
-import { getAllAuthors } from "./service/service";
+import { getAllAuthors, createAuthor } from "./services/service";
 import { NextResponse } from 'next/server';
 
 export async function GET(request: Request) {
@@ -10,4 +10,24 @@ export async function GET(request: Request) {
       "Content-Type": "application/json",
     },
   });
+}
+
+export async function POST(request: Request) {
+  const body = await request.json();
+  const newAuthor = await createAuthor(body);
+  if (newAuthor.status === 'error') {
+    return new NextResponse(JSON.stringify(newAuthor), {
+      status: 409,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  } else if (newAuthor.status === 'success'){
+    return new NextResponse(JSON.stringify(newAuthor), {
+      status: 201,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  }
 }
