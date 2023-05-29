23ab37ae668ad704372292721ad7f0cf10f36ff7
iamlucasgomes <lucas.devjs@gmail.com>
2023-05-29 14:32:40 -0300

    feat(provider): create Provider

---
+++ src/contexts/Provider.tsx
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import Props from "./interfaces/Props";
+import MyContextData from "./interfaces/MyContextData";
+import AppContext from "./Context";
+
+
+const Provider = ({ children }: Props) => {
+
+  const [isAddingAuthor, setAddingAuthor] = useState(false);
+
+  const context: MyContextData = {
+    isAddingAuthor,
+    setAddingAuthor,
+  };
+
+  return <AppContext.Provider value={context}>
+    {children}
+  </AppContext.Provider>
+
+}
+
+export default Provider;
