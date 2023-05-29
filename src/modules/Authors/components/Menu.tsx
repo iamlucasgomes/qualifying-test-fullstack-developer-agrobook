aeff3ae6f4f875087871cb4ceec87b9c596e52ad
iamlucasgomes <lucas.devjs@gmail.com>
2023-05-29 15:06:00 -0300

    refactor(menu authors): add onMenuClick function in Menu Component

--- src/modules/Authors/components/Menu.tsx
+++ src/modules/Authors/components/Menu.tsx
@@ -1,14 +1,17 @@
-import React, { MouseEventHandler } from 'react';
+import React from 'react';
 import { Button, Space } from 'antd';
+import { useAppContext } from '@/hooks/useAppContext';
 
-const onMenuClick: MouseEventHandler<HTMLAnchorElement> & MouseEventHandler<HTMLButtonElement> = (e) => {
-  console.log('click', 'vou ser uma action');
+
+const App: React.FC = () => {
+  const { setAddingAuthor } = useAppContext();
+  const onMenuClick = () => setAddingAuthor(true);
+
+  return (
+    <Space direction="horizontal">
+      <Button onClick={onMenuClick} type="primary">Adicionar Autor</Button>
+    </Space>
+  )
 };
-
-const App: React.FC = () => (
-  <Space direction="horizontal">
-    <Button onClick={onMenuClick} type="primary">Adicionar Autor</Button>
-  </Space>
-);
 
 export default App;
