92294c3466db2b24f00ba879912e1fb70fb23c82
iamlucasgomes <lucas.devjs@gmail.com>
2023-05-30 13:39:23 -0300

    refactor(myContextData): added isAddingBook property to myContextData interface

--- src/contexts/interfaces/MyContextData.ts
+++ src/contexts/interfaces/MyContextData.ts
@@ -3,4 +3,6 @@ import React from "react";
 export default interface MyContextData {
   isAddingAuthor: boolean;
   setAddingAuthor: React.Dispatch<React.SetStateAction<boolean>>;
+  isAddingBook: boolean;
+  setIsAddingBook: React.Dispatch<React.SetStateAction<boolean>>;
 }
