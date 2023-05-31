072fab60693d03d12d76316ecebd27ad85157459
iamlucasgomes <lucas.devjs@gmail.com>
2023-05-31 15:08:17 -0300

    feat: create home page

--- src/modules/Home/Home.tsx
+++ src/modules/Home/Home.tsx
@@ -1,9 +1,12 @@
 import React from "react";
+import { Typography } from 'antd';
+
+const { Title } = Typography;
 
 const Home: React.FC = () => {
   return (
     <div>
-      <p>Bem vindo a Livraria</p>
+      <Title style={{color: '#1677FF'}}>Bem vindo a Livraria</Title>
     </div>
   );
 };
