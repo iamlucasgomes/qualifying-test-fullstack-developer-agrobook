e6d63114c2051907f4cd99896f352f2109dc8087
iamlucasgomes <lucas.devjs@gmail.com>
2023-05-30 22:43:55 -0300

    revert(books): reverts interface to original state

--- src/modules/Books/interfaces/Books.ts
+++ src/modules/Books/interfaces/Books.ts
@@ -1,8 +1,8 @@
 export default interface Books {
   id?: number;
-  nome?: string;
-  data_lancamento?: string;
-  descricao?: string;
-  categoria?: string;
-  autores?: string[];
+  nome: string;
+  data_lancamento: string;
+  descricao: string;
+  categoria: string;
+  autores: string[];
 }
