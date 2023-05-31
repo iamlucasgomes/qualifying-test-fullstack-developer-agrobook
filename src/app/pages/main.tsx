4b575c939a62503b505eedef8b8506ec1dd33e62
iamlucasgomes <lucas.devjs@gmail.com>
2023-05-31 03:24:49 -0300

    feat: add loader in main page

--- src/app/pages/main.tsx
+++ src/app/pages/main.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   HomeOutlined,
   UserOutlined,
@@ -48,10 +48,17 @@ const App: React.FC = () => {
     token: { colorBgContainer },
   } = theme.useToken();
 
-  const handleLoad = () => {
-    setLoading(false);
-  };
+  useEffect(() => {
+    const handleLoad = () => {
+      setLoading(false);
+    };
 
+    window.addEventListener('load', handleLoad);
+
+    return () => {
+      window.removeEventListener('load', handleLoad);
+    };
+  }, []);
   const handleMenuItemSelect = ({ keyPath }: { keyPath: React.Key[] }) => {
     const selectedKey = keyPath[keyPath.length - 1];
     const selectedMenuItem = items.find((item) => item?.key === selectedKey);
@@ -80,7 +87,7 @@ const App: React.FC = () => {
   }
 
   return (
-    <Layout onLoad={handleLoad} style={{ minHeight: '100vh' }}>
+    <Layout style={{ minHeight: '100vh' }}>
       <Sider collapsible collapsed={collapsed} onCollapse={(value) => setCollapsed(value)}>
         <div className="demo-logo-vertical" />
         <Menu
